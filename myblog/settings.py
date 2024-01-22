548a935798e7b8b43073e21c67868b0ced117128
chatchai junpatch <chatchai.ju@ku.th>
2024-01-22 21:38:56 +0700

    update s3 storage

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mm9q2o+c9_el_m6r#7(zb2x4eu-(9wkg4qtuggq@ij8_xxmi$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.170.185.238','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
